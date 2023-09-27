240a305837a41b65d6621ae4686feb34cc3c006a
Sebastián Acevedo <sbsacev@gmail.com>
2023-09-27 12:46:06 -0300

    cambio de carpetas

--- cap01/02numeros.py
+++
@@ -1,3 +0,0 @@
-#esto es un comentario
-edaduniverso=14_000_000_000
-print(edaduniverso)
---
+++ cap02/02numeros.py
@@ -0,0 +1,11 @@
+#esto es un comentario
+edaduniverso=14_000_000_000
+print(edaduniverso)
+a=4
+b=5
+c=8
+d=10
+print("suma:",a+b)
+print("resta:",c-b)
+print("multiplación:",c*d)
+print("división:",d/c)
