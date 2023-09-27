18090afe4aace52fe20acbf5c9354d046348e74f
Sebastián Acevedo <sbsacev@gmail.com>
2023-09-27 12:25:07 -0300

    edito archivo inicial

--- cap01/test.py
+++ cap01/test.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+mensaje = "Hola todos, que tal?"
+print(mensaje)
+nombre = "seba acevedo"
+print(nombre.upper())
+apellido2 = "alvarez"
+completo=f"{nombre} {apellido2}"
+print("Hola,",completo.title())
