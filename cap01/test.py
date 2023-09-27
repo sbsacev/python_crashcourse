60c800ec90deb8bba7f035b29be16b9002b28378
Sebastián Acevedo <sbsacev@gmail.com>
2023-09-27 12:29:58 -0300

    cambio minimo

--- cap01/test.py
+++ cap01/test.py
@@ -3,5 +3,7 @@ print(mensaje)
 nombre = "seba acevedo"
 print(nombre.upper())
 apellido2 = "alvarez"
-completo=f"{nombre} {apellido2}"
+ciudad = "Valdivia"
+pais = "Chile"
+completo=f"{nombre} {apellido2}\n\t{ciudad}\n{pais}"
 print("Hola,",completo.title())
