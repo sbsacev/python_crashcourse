d6602efd40ee71799538ebcc6ce51a1bf0200305
Sebastián Acevedo <sbsacev@gmail.com>
2023-10-03 16:35:29 -0300

    nuevos archivos

---
+++ cap04/05_listas2.py
@@ -0,0 +1,7 @@
+cosas = ['celu','tazón','cuchara']
+for cosa in cosas:
+    print(cosa)
+for cosa in cosas:
+    print(f'Tengo: {cosa.title()}')
+print('¿Hola qué tal?')
+#importancia de la indentación
---
+++ cap04/06_numeroslists.py
@@ -0,0 +1,6 @@
+for value in range(0,5):
+    print(value)
+for value in range(10):
+    print(value)
+numeros=list(range(5,10,2)) #el tercer argumento es opcional, indica el paso
+print(numeros)
